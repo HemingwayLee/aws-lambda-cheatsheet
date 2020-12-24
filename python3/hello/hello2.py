02396f96f153293deb615ee22c9c14b7741f65e3
LeeHemingway <coherence0815@gmail.com>
2020-12-25 06:22:30 +0900

    add another example

---
+++ python3/hello/hello2.py
@@ -0,0 +1,9 @@
+def handler(event, context):
+    print(f"hello, {event['answer']}")
+    
+    print("Lambda function ARN:", context.invoked_function_arn)
+    print("CloudWatch log stream name:", context.log_stream_name)
+    print("CloudWatch log group name:",  context.log_group_name)
+    print("Lambda Request ID:", context.aws_request_id)
+
+    return 5566
